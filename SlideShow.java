542018ee2c88e17f999f086b9828aafe85e6bf2a
Tevaen Kenerly <tevaenkenerly1@gmail.com>
2025-10-01 21:53:24 -0400

    Add files via upload

---
+++ SlideShow.java
@@ -0,0 +1,174 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class SlideShow extends JFrame {
+
+	//Declare Variables
+	private JPanel slidePane;
+	private JPanel textPane;
+	private JPanel buttonPane;
+	private CardLayout card;
+	private CardLayout cardText;
+	private JButton btnPrev;
+	private JButton btnNext;
+	private JLabel lblSlide;
+	private JLabel lblTextArea;
+
+	/**
+	 * Create the application.
+	 */
+	public SlideShow() throws HeadlessException {
+		initComponent();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	// i updated the background color of the text with yellow as the blue made the text hard to see, the yellow helps with readability
+	// i did so by finding colors that could be implemented with the COLOR function that is used here
+	private void initComponent() {
+		//Initialize variables to empty objects
+		card = new CardLayout();
+		cardText = new CardLayout();
+		slidePane = new JPanel();
+		textPane = new JPanel();
+		textPane.setBackground(Color.YELLOW);
+		textPane.setBounds(5, 470, 790, 50);
+		textPane.setVisible(true);
+		buttonPane = new JPanel();
+		btnPrev = new JButton();
+		btnNext = new JButton();
+		lblSlide = new JLabel();
+		lblTextArea = new JLabel();
+
+		//Setup frame attributes
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setTitle("Top 5 Destinations SlideShow");
+		getContentPane().setLayout(new BorderLayout(10, 50));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Setting the layouts for the panels
+		slidePane.setLayout(card);
+		textPane.setLayout(cardText);
+		
+		//logic to add each of the slides and text
+		for (int i = 1; i <= 5; i++) {
+			lblSlide = new JLabel();
+			lblTextArea = new JLabel();
+			lblSlide.setText(getResizeIcon(i));
+			lblTextArea.setText(getTextDescription(i));
+			slidePane.add(lblSlide, "card" + i);
+			textPane.add(lblTextArea, "cardText" + i);
+		}
+
+		getContentPane().add(slidePane, BorderLayout.CENTER);
+		getContentPane().add(textPane, BorderLayout.SOUTH);
+
+		buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+		btnPrev.setText("Previous");
+		btnPrev.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goPrevious();
+			}
+		});
+		buttonPane.add(btnPrev);
+
+		btnNext.setText("Next");
+		btnNext.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goNext();
+			}
+		});
+		buttonPane.add(btnNext);
+
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Previous Button Functionality
+	 */
+	private void goPrevious() {
+		card.previous(slidePane);
+		cardText.previous(textPane);
+	}
+	
+	/**
+	 * Next Button Functionality
+	 */
+	private void goNext() {
+		card.next(slidePane);
+		cardText.next(textPane);
+	}
+
+	/**
+	 * Method to get the images
+	 */
+	// I added new images to the slideshow by downloading jpeg images onto my compute copy and pasting them into the resources folder
+	// I then updated the code by adding the names of the jpg files from the resources folder
+	private String getResizeIcon(int i) {
+		String image = ""; 
+		if (i==1){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/TestImage1.jpg") + "'</body></html>";
+		} else if (i==2){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Yosemite.jpg") + "'</body></html>";
+		} else if (i==3){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Yellowstone.jpg") + "'</body></html>";
+		} else if (i==4){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Maui.jpg") + "'</body></html>";
+		} else if (i==5){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Laketahoe.jpg") + "'</body></html>";
+		}
+		return image;
+	}
+	
+	/**
+	 * Method to get the text values
+	 */
+	// the client wanted to focus on the wellness/detox of each destination, i added wellness/detox benefits that each location offered
+	// I did this by adding text along with the original one sentence description 
+	private String getTextDescription(int i) {
+		String text = ""; 
+		if (i==1){
+			text = "<html><body><font size='5'>#1 The Grand Canyon.</font> <br>Not only do you get spectacular canyon views and hiking, but this also comes with movement, fresh air, and is designed to awaken the mind, body, and spirit.</body></html>";
+		} else if (i==2){
+			text = "<html><body><font size='5'>#2 Yosemite National Park.</font> <br>Surrounded by waterfalls, cliffs, and ancient forests, Yosemite is a haven for physical and emotional renewal.</body></html>";
+		} else if (i==3){
+			text = "<html><body><font size='5'>#3 Yellowstone National Park.</font> <br>Geyers, wildlife, and geothermal wonders, as the nature helps naturally boost your mood and the hot springs supports muscle recovery.</body></html>";
+		} else if (i==4){
+			text = "<html><body><font size='5'>#4 Maui, HI.</font> <br>Iconic beaches, volcanoes, and lush landscapes, paired with ocean threapy, tropical fruits, and yoga, massage, and spiritual healings!</body></html>";
+		} else if (i==5){
+			text = "<html><body><font size='5'>#5 Lake Tahoe.</font> <br>Crystal-clear lake, forests, and alpine beauty, provides lakeside meditation, forest bathing, and clean eating.</body></html>";
+		}
+		return text;
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				SlideShow ss = new SlideShow();
+				ss.setVisible(true);
+			}
+		});
+	}
+}
